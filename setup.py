8a39e632be4dd8010b4af741ccabdc6a7ebd37b9
pentest-gg <zumtestenfuerdiscord@gmail.com>
2025-09-04 01:34:43 +0200

    Prepare release on pypi

--- setup.py
+++ setup.py
@@ -1,40 +1,50 @@
-from setuptools import setup, find_packages
+from pathlib import Path
+
+from setuptools import find_packages, setup
 
 install_requires = [
-    'aiobotocore>=2.24.1',
+    "aiobotocore>=2.24.1",
     'typing-extensions>=4; python_version<"3.11"',
 ]
 
 setup(
-    name='aiopynamodb',
-    version=__import__('aiopynamodb').__version__,
-    packages=find_packages(exclude=('examples', 'tests', 'typing_tests', 'tests.integration',)),
-    url='http://jlafon.io/pynamodb.html',
+    name="as-aiopynamodb",
+    version=__import__("aiopynamodb").__version__,
+    packages=find_packages(
+        exclude=(
+            "examples",
+            "tests",
+            "typing_tests",
+            "tests.integration",
+        )
+    ),
+    url="http://jlafon.io/pynamodb.html",
     project_urls={
-        'Source': 'https://github.com/brunobelloni/AioPynamoDB',
+        "Fork": "https://github.com/AppSolves/AioPynamoDB",
+        "Original": "https://github.com/brunobelloni/AioPynamoDB",
     },
-    author='Jharrod LaFon',
-    author_email='jlafon@eyesopen.com',
-    description='An Async Pythonic Interface to DynamoDB',
-    long_description=open('README.rst').read(),
-    long_description_content_type='text/x-rst',
+    author="Jharrod LaFon",
+    author_email="jlafon@eyesopen.com",
+    description="An Async Pythonic Interface to DynamoDB",
+    long_description=Path("README.rst").read_text("utf-8"),
+    long_description_content_type="text/x-rst",
     zip_safe=False,
-    license='MIT',
-    keywords='python dynamodb amazon async asyncio aiobotocore',
+    license="MIT",
+    keywords="python dynamodb amazon async asyncio aiobotocore",
     python_requires=">=3.10",
     install_requires=install_requires,
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'License :: OSI Approved :: MIT License',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
     ],
     extras_require={
-        'signals': ['blinker>=1.3,<2.0'],
+        "signals": ["blinker>=1.3,<2.0"],
     },
-    package_data={'aiopynamodb': ['py.typed']},
+    package_data={"aiopynamodb": ["py.typed"]},
 )
